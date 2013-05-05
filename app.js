d4d8de0f16fb43f8c3addd8a04c9affe2b1db38c
Ben Evans <ben@bensbit.co.uk>
2013-05-05 22:05:34 +0100

    styleless poll for embed

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ app.get('*', function(req, res, next) {
 
 app.get('/', routes.index);
 app.get('/poll/:id', routes.poll);
-
+app.get('/poll/:id/simple', routes.poll.simple);
 app.get('/admin', isAdmin, function(req, res) {
   res.redirect('/admin/poll');
 });
--- routes/poll.js
+++ routes/poll.js
@@ -1,6 +1,14 @@
 
-var db = require('../db');
+/**
+ * Dependencies
+ */
 
+var db = require('../db'),
+    _ = require('underscore');
+
+/**
+ * GET /poll/:id
+ */
 module.exports = function(req, res, next) {
   db.Poll.find({
     where: {
@@ -9,5 +17,19 @@ module.exports = function(req, res, next) {
   }).success(function(poll) {
     if(!poll) return next();
     res.render('poll', poll);
+  }).error(next);
+};
+
+/**
+ * GET /poll/:id/simple
+ */
+module.exports.simple = function(req, res, next) {
+  db.Poll.find({
+    where: {
+      id: req.params.id
+    }
+  }).success(function(poll) {
+    if(!poll) return next();
+    res.render('poll', _.extend(poll, {layout:false}));
   }).error(next);
 };
